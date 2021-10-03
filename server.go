4224f1fd499598569146fa7d3a5af5f8e5c6faa2
dirayser <dirayser@gmail.com>
2021-10-03 17:15:14 +0300

    setup server on localhost

--- server.go
+++ server.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type date struct {
+	Time string `json:"time"`
+}
+
 func main() {
+	http.HandleFunc("/time", handleTime)
+	fmt.Printf("Listening at port 8795\n")
+	http.ListenAndServe(":8795", nil)
+}
+
+func handleTime(w http.ResponseWriter, req *http.Request) {
+	var currentTime = time.Now().Format(time.RFC3339)
+	var response = date{currentTime}
+	var jsonResponse, error = json.Marshal(&response)
+	if error == nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonResponse)
+	}
 }
