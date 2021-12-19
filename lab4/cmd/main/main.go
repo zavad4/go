f6ec9d8e926b7f577b15f8b430efe9f395ea47ae
zavad4 <lizavodovska@gmail.com>
2021-12-19 20:42:58 +0200

    add main and input files

---
+++ lab4/cmd/main/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/zavad4/go/lab4/commands"
+	"github.com/zavad4/go/lab4/engine"
+)
+
+func main() {
+	path := "./example.txt"
+	file := flag.String("f", path, "Input commands")
+	flag.Parse()
+
+	input, err := os.Open(*file)
+	if err != nil {
+		log.Fatalf("Impossible to open file:  %s", err)
+		return
+	}
+	defer input.Close()
+
+	eventLoop := new(engine.EventLoop)
+	eventLoop.Start()
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		commandLine := scanner.Text()
+		cmd := commands.Parse(commandLine)
+		eventLoop.Post(cmd)
+	}
+
+	eventLoop.AwaitFinish()
+}
