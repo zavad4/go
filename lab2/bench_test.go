c617954e627780685612643c33fdfd43be62f8ef
zavad4 <lizavodovska@gmail.com>
2021-12-25 15:08:40 +0200

    update benchmark

--- lab2/bench_test.go
+++ lab2/bench_test.go
@@ -2,26 +2,31 @@ package lab2
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 )
 
-var startStatement string = "2 1 +"
+var startStatement string = "1 2 +"
 var cntRes string
 var err error
 
 func BenchmarkPostfixToInfix(b *testing.B) {
-	const baseLen = 2500
+	const baseLen = 150
+	iterationsNum := baseLen
+	
+	signs := [4]string{"+", "-", "*", "/"}
+	numbers := [10]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 
-	for i := 0; i < 25; i++ {
+	for i := 0; i < 20; i++ {
+		iterationsNum = iterationsNum * 5 / 4
 		input := startStatement
-		iterationsNum := baseLen * (i + 1)
 
 		for j := 0; j < iterationsNum; j++ {
-			input = "+ " + input
-			input = input + " "
-			input = input + startStatement
+			number := numbers[rand.Intn(10)]
+			sign := signs[rand.Intn(4)]
+	
+			input = input + " " + number + " " + sign
 		}
-
 		b.Run(fmt.Sprintf("len=%d", iterationsNum), func(b *testing.B) {
 			cntRes, err = PostfixToInfix(input)
 		})
